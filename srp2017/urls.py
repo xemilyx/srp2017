9c54889f4f2ef9e41aba13c6084884a4687c97b6
xemilyx <emilybrown@cranstonlibrary.org>
2017-04-11 17:08:45 -0400

    Adding about and contact pages, plus navigation.

--- srp2017/urls.py
+++ srp2017/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
+        # The new URL entries we're adding:
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
+        
     url(r'^admin/', admin.site.urls),
 ]
